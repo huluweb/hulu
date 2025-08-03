52bad9acc98c5c384f23cf7bd6dc2b7f997400bf
huluweb <zgju9781@gmail.com>
2025-08-03 21:15:51 +0300

    Update axiosInstance.ts

--- src/helper/axiosInstance.ts
+++ src/helper/axiosInstance.ts
@@ -13,7 +13,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = Bearer ${token}; // Fixed: Use backticks for template literal
+      config.headers.Authorization = Bearer ${token}; // Use backticks for template literal
     }
     return config;
   },
